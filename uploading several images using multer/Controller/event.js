19c4a64b48883b1c50da1594247fb7dadac6530f
jayant vashisth <vashisthjayant@gmail.com>
2022-10-12 16:20:37 +0530

    code updated

--- uploading several images using multer/Controller/event.js
+++ uploading several images using multer/Controller/event.js
@@ -4,14 +4,14 @@ const Events = require('../Schema/Events')
 exports.createEvent = async (req, res) => {
 
     try {
-        
+
         const { title, date, timings, description, whyThisEvent, sName, sDesignation, sLinkedIn } = req.body;
         const speaker = {
-            name:sName,
-            designation:sDesignation,
-            linkedIn:sLinkedIn,
-            image:"",
-            photo:""
+            name: sName,
+            designation: sDesignation,
+            linkedIn: sLinkedIn,
+            image: "",
+            photo: ""
         }
         const event = new Events({
             title, date, timings, description, whyThisEvent, speaker
@@ -54,7 +54,7 @@ exports.delteEvent = async (req, res) => {                 //up and working
         if (!event) { return res.status(404).send("Not Found") }
 
         event = await Events.findByIdAndDelete(req.params.id)
-        res.status(200).json({ "Success": "Event have been deleted"});
+        res.status(200).json({ "Success": "Event have been deleted" });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
@@ -73,6 +73,40 @@ exports.getEventByid = async (req, res) => {                 //up and working
         res.status(500).send("Internal Server Error");
     }
 }
+
+
+exports.updateEventById = async (req, res) => {         //up and working
+
+    try {
+        const { title, date, timings, description, whyThisEvent, sName, sDesignation, sLinkedIn } = req.body;
+        
+        let event = await Events.findById(req.params.id);
+        if (!event) { return res.status(404).send("Not Found") }
+        
+
+        event.title = title
+        event.Date = date
+        event.timings = timings
+        event.description = description
+        event.whyThisEvent = whyThisEvent
+        event.speaker.name = sName
+        event.speaker.designation = sDesignation
+        event.speaker.linkedIn = sLinkedIn
+
+        if (req.files) {
+            event.image = req.files.image[0].path
+            event.speaker.image = req.files.sImage[0].path
+            event.speaker.photo = req.files.sPhoto[0].path
+        }
+
+        event = await Events.findByIdAndUpdate(req.params.id, { $set: event }, { new: true })
+        res.json({ event });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+}
+
 
 
 
--- uploading several images using multer/Routes/event.js
+++ uploading several images using multer/Routes/event.js
@@ -6,16 +6,19 @@ const createEvent = require('../Controller/event').createEvent
 const getAllEvent = require('../Controller/event').getAllEvent
 const deleteEvent = require('../Controller/event').delteEvent
 const getEventByid = require('../Controller/event').getEventByid
+const updateEventById = require('../Controller/event').updateEventById
 
 
-router.post('/createEvent', upload.fields([{name:'image', maxCount: 1},{name:'sImage', maxCount: 1}, {name:'sPhoto', maxCount: 1}]), createEvent)
+router.post('/createEvent', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'sImage', maxCount: 1 }, { name: 'sPhoto', maxCount: 1 }]), createEvent)
 
 
-router.get('/getAllEvent',getAllEvent)                      //up and working
+router.get('/getAllEvent', getAllEvent)                      //up and working
 
-router.get('/getEventByid/:id',getEventByid)                      //up and working
+router.get('/getEventByid/:id', getEventByid)                      //up and working
 
 router.delete('/deleteEvent/:id', deleteEvent)             //up and working
+
+router.put('/updateEventById/:id', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'sImage', maxCount: 1 }, { name: 'sPhoto', maxCount: 1 }]), updateEventById)             //up and working
 
 
 
